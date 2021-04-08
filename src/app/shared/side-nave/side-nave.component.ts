9a4d7250d504f03459e75de34626fa1c8febaa9d
atulmahindra <atulunitech@gmail.com>
2021-04-09 01:18:18 +0530

    login test

--- src/app/shared/side-nave/side-nave.component.ts
+++ src/app/shared/side-nave/side-nave.component.ts
@@ -14,37 +14,37 @@ export class SideNaveComponent implements OnInit {
   options = { multi: false };
   menus = [
    { 
-     name: 'Front-end',
+     name: 'Menu 1',
      iconClass: 'fa fa-code',
      active: true,
      submenu: [
-       { name: 'HTML', url: '#' },
-       { name: 'CSS', url: '#' },
-       { name: 'Javascript', url: '#' }
+       { name: 'Sub Menu 1 ', url: '#' },
+       { name: 'Sub Menu 1', url: '#' },
+       { name: 'Sub Menu 1', url: '#' }
      ]
    },
    { 
-     name: 'Responsive web',
+     name: 'Menu 2',
      iconClass: 'fa fa-mobile',
      active: false,
      submenu: [
-       { name: 'Tablets', url: '#' },
-       { name: 'Mobiles', url: '#' },
-       { name: 'Desktop', url: '#' }
+       { name: 'Sub Menu 2', url: '#' },
+       { name: 'Sub Menu 2', url: '#' },
+       { name: 'Sub Menu 2', url: '#' }
      ]
    },
    { 
-     name: 'Web Browser',
+     name: 'Menu 3',
      iconClass: 'fa fa-globe',
      active: false,
      submenu: [
-       { name: 'Chrome', url: '#' },
-       { name: 'Firefox', url: '#' },
-       { name: 'Desktop', url: '#' }
+       { name: 'Sub Menu 3', url: '#' },
+       { name: 'Sub Menu 3', url: '#' },
+       { name: 'Sub Menu 3', url: '#' }
      ]
    },
    { 
-     name: 'Web Browser',
+     name: 'Menu 4',
      iconClass: 'fa fa-globe',
      active: false
    }
