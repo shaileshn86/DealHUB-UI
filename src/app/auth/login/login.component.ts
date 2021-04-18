7311c7328f0465c1e1f6548ff7676d784b1cfb1a
atulunitech <atulunitech@gmail.com>
2021-04-18 14:11:25 +0530

    login changes

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +7,19 @@ import {FormControl, Validators} from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  public loginvalid: FormGroup;
+  log_in:boolean=true;
+  lost_pass:boolean= false;
   constructor() { }
 
   ngOnInit(): void {
     // sample comment
+    this.loginvalid = new FormGroup({
+     
+      userID : new FormControl('', [Validators.required]),
+      Password : new FormControl('', [Validators.required])
+
+    });
   }
   email = new FormControl('', [Validators.required, Validators.email]);
 
@@ -22,6 +30,20 @@ export class LoginComponent implements OnInit {
 
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
+
+  public checkError = (controlName: string, errorName: string) => {
+    return this.loginvalid.controls[controlName].hasError(errorName);
+  }
   
+  lostPass()
+  {
+    this.log_in = false;
+    this.lost_pass = true;
+  }
+  relogin()
+  {
+    this.log_in = true;
+    this.lost_pass = false;
+  }
 }
 
